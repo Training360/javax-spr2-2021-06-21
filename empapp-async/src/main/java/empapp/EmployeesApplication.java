c030349489e95b98044ca1462ab594e10eb78339
Istvan Viczian <viczian.istvan@gmail.com>
2021-06-21 16:38:58 +0200

    Async init projekt

---
+++ empapp-async/src/main/java/empapp/EmployeesApplication.java
@@ -0,0 +1,18 @@
+package empapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@Configuration
+@EnableAsync
+public class EmployeesApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+}
