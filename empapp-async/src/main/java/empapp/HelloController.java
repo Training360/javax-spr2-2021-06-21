ac49c2ad91d76177460effc403885a3ab079a586
Istvan Viczian <viczian.istvan@gmail.com>
2021-06-21 23:58:06 +0200

    Async és ütemezése

--- empapp-async/src/main/java/empapp/HelloController.java
+++ empapp-async/src/main/java/empapp/HelloController.java
@@ -1,6 +1,7 @@
 package empapp;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/hello")
 @AllArgsConstructor
+@Slf4j
 public class HelloController {
 
     private HelloService helloService;
 
     @GetMapping
     public String sayHello() {
+        log.info(Thread.currentThread().getName());
         helloService.sayHello();
         return "Hello World!";
     }
--- empapp-async/src/main/java/empapp/HelloService.java
+++ empapp-async/src/main/java/empapp/HelloService.java
@@ -2,6 +2,7 @@ package empapp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +11,7 @@ public class HelloService {
 
     @Async
     public void sayHello() {
+        log.info(Thread.currentThread().getName());
         for (int i = 0; i < 10; i++) {
             log.info("Waiting " + i);
             try {
@@ -19,5 +21,11 @@ public class HelloService {
                 throw new IllegalArgumentException("Error", ie);
             }
         }
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void sayHelloAtFixedRate() {
+        log.info(Thread.currentThread().getName());
+        log.info("Say hello from scheduler!");
     }
 }
