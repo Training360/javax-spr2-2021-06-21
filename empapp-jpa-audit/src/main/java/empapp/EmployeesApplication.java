03c36f540a618500e1ecd9741cefe57f4d3e6549
Istvan Viczian <viczian.istvan@gmail.com>
2021-06-21 14:44:02 +0200

    Spring Data JPA Audit

--- empapp-jpa-audit/src/main/java/empapp/EmployeesApplication.java
+++ empapp-jpa-audit/src/main/java/empapp/EmployeesApplication.java
@@ -3,9 +3,11 @@ package empapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @Configuration
+@EnableJpaAuditing
 public class EmployeesApplication
 {
 
