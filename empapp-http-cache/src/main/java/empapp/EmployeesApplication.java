59776a5a5f2f18eea8922dd2648a75abe086d795
Istvan Viczian <viczian.istvan@gmail.com>
2021-06-22 14:03:02 +0200

    HTTP cache

--- empapp-http-cache/src/main/java/empapp/EmployeesApplication.java
+++ empapp-http-cache/src/main/java/empapp/EmployeesApplication.java
@@ -2,7 +2,10 @@ package empapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 @Configuration
@@ -11,6 +14,15 @@ public class EmployeesApplication
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<ShallowEtagHeaderFilter> etagFilter(){
+		FilterRegistrationBean<ShallowEtagHeaderFilter> registrationBean
+				= new FilterRegistrationBean<>();
+		registrationBean.setFilter(new ShallowEtagHeaderFilter());
+		registrationBean.addUrlPatterns("/js/*");
+		return registrationBean;
 	}
 
 }
