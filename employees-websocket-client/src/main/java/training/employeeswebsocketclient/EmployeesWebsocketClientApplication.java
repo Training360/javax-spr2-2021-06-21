d5b8eed9767ce255d424f7c6995441b0b852a090
Istvan Viczian <viczian.istvan@gmail.com>
2021-06-28 10:11:53 +0200

    WebSocket üzenetküldés

--- employees-websocket-client/src/main/java/training/employeeswebsocketclient/EmployeesWebsocketClientApplication.java
+++ employees-websocket-client/src/main/java/training/employeeswebsocketclient/EmployeesWebsocketClientApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
@@ -15,6 +17,7 @@ import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @Slf4j
@@ -33,8 +36,13 @@ public class EmployeesWebsocketClientApplication implements CommandLineRunner {
 		WebSocketStompClient stompClient = new WebSocketStompClient(client);
 		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 		StompSessionHandler sessionHandler = new MessageStompSessionHandler();
-		stompClient.connect("ws://localhost:8080/websocket-endpoint", sessionHandler);
+		ListenableFuture<StompSession> future = stompClient.connect("ws://localhost:8080/websocket-endpoint", sessionHandler);
+		StompSession session = future.get(5000, TimeUnit.MILLISECONDS);
 		System.out.println("Press any key to exit");
-		new Scanner(System.in).nextLine();
+		String line = new Scanner(System.in).nextLine();
+		while (!line.trim().equals("")) {
+			session.send("/app/messages", new MessageCommand(line));
+			line = new Scanner(System.in).nextLine();
+		}
 	}
 }
---
+++ employees-websocket-client/src/main/java/training/employeeswebsocketclient/MessageCommand.java
@@ -0,0 +1,13 @@
+package training.employeeswebsocketclient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageCommand {
+
+    private String content;
+}
