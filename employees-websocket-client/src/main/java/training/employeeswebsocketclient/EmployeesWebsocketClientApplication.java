73c0e076b12a42eddd61671cddea825d7096440a
Istvan Viczian <viczian.istvan@gmail.com>
2021-06-28 09:58:46 +0200

    WebSocket client

--- employees-websocket-client/src/main/java/training/employeeswebsocketclient/EmployeesWebsocketClientApplication.java
+++ employees-websocket-client/src/main/java/training/employeeswebsocketclient/EmployeesWebsocketClientApplication.java
@@ -4,6 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 @Slf4j
@@ -16,5 +27,14 @@ public class EmployeesWebsocketClientApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Started...");
+		//WebSocketClient client = new StandardWebSocketClient();
+		WebSocketClient client = new SockJsClient(
+				Arrays.asList(new WebSocketTransport(new StandardWebSocketClient())));
+		WebSocketStompClient stompClient = new WebSocketStompClient(client);
+		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+		StompSessionHandler sessionHandler = new MessageStompSessionHandler();
+		stompClient.connect("ws://localhost:8080/websocket-endpoint", sessionHandler);
+		System.out.println("Press any key to exit");
+		new Scanner(System.in).nextLine();
 	}
 }
---
+++ employees-websocket-client/src/main/java/training/employeeswebsocketclient/MessageDto.java
@@ -0,0 +1,9 @@
+package training.employeeswebsocketclient;
+
+import lombok.Data;
+
+@Data
+public class MessageDto {
+
+    private String text;
+}
