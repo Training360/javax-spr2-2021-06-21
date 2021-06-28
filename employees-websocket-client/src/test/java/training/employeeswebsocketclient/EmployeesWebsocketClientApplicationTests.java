4e157e0b7c5301a4013e958b8ca521ea4f92e37f
Istvan Viczian <viczian.istvan@gmail.com>
2021-06-28 09:43:28 +0200

    Parancssoros kliens

---
+++ employees-websocket-client/src/test/java/training/employeeswebsocketclient/EmployeesWebsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package training.employeeswebsocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesWebsocketClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
