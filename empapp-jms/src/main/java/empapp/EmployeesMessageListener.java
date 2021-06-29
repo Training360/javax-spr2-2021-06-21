ca50d9e68649923efe12eaedbebcef3efedc80da
Istvan Viczian <viczian.istvan@gmail.com>
2021-06-29 12:10:30 +0200

    Kapcsolódás különálló Artemishez

--- empapp-jms/src/main/java/empapp/EmployeesMessageListener.java
+++ empapp-jms/src/main/java/empapp/EmployeesMessageListener.java
@@ -4,17 +4,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
 @Service
 @Slf4j
 public class EmployeesMessageListener {
 
-    @JmsListener(destination = "employeesQueue")
-    public void handleMessage(EmployeeHasBeenCreatedDto message) {
-        log.info("Message has been arrived: " + message.getMessage());
-    }
+    @JmsListener(destination = "eventsQueue")
+//    public void handleMessage(EmployeeHasBeenCreatedDto message) {
+//        log.info("Message has been arrived: " + message.getMessage());
+//    }
 
-    @JmsListener(destination = "employeesQueue")
-    public void handleMessage2(EmployeeHasBeenCreatedDto message) {
-        log.info("Message has been arrived 2: " + message.getMessage());
+    public void handleMessage(Message message) throws JMSException {
+        log.info("Message has been arrived: " + message);
+        log.info(message.getJMSMessageID());
+        log.info(Boolean.toString(message.getJMSRedelivered()));
+        log.info(Integer.toString(message.getIntProperty("JMSXDeliveryCount")));
+
+        throw new IllegalArgumentException("Invalid message");
     }
 }
